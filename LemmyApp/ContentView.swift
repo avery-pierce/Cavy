c3ca5d02af790122a9dcbf56cfc3145cadf1a32f
Avery Pierce <avery.pierce@pm.me>
2020-10-08 21:24:37 -0500

    Initial Commit

---
+++ LemmyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LemmyApp
+//
+//  Created by Avery Pierce on 10/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LemmyApp/LemmyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemmyAppApp.swift
+//  LemmyApp
+//
+//  Created by Avery Pierce on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct LemmyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
