e310ee4e9943e5a9662ee21e9744aee32a827320
Avery Pierce <avery.pierce@pm.me>
2021-01-30 11:19:37 -0600

    Add coding keys

--- LemmyApp/Lemmy Entities/LemmyCommunitySummary.swift
+++ LemmyApp/Lemmy Entities/LemmyCommunitySummary.swift
@@ -12,5 +12,33 @@ struct LemmyCommunitySummary: Codable {
     var creator: LemmyUser?
     var category: LemmyCategory?
     var subscribed: Bool?
-    var counts: LemmyCounts?
+    var counts: Counts
+    
+    struct Counts: Codable {
+        var id: Int
+        var communityID: Int
+        var subscribers: Int?
+        var posts: Int?
+        var comments: Int?
+        var published: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case id = "id"
+            case communityID = "community_id"
+            case subscribers = "subscribers"
+            case posts = "posts"
+            case comments = "comments"
+            case published = "published"
+        }
+        
+        init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            id = try values.decode(Int.self, forKey: .id)
+            communityID = try values.decode(Int.self, forKey: .communityID)
+            subscribers = try values.decodeIfPresent(Int.self, forKey: .subscribers)
+            posts = try values.decodeIfPresent(Int.self, forKey: .posts)
+            comments = try values.decodeIfPresent(Int.self, forKey: .comments)
+            published = try values.decodeIfPresent(String.self, forKey: .published)
+        }
+    }
 }
--- LemmyApp/Lemmy Entities/LemmyPostItemSummary.swift
+++ LemmyApp/Lemmy Entities/LemmyPostItemSummary.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 struct LemmyPostItemSummary: Codable {
-    // TODO: CodingKeys
     var post: LemmyPostItem?
     var creator: LemmyUser?
     var community: LemmyCommunity?
@@ -19,10 +18,33 @@ struct LemmyPostItemSummary: Codable {
     var read: Bool?
     var myVote: Bool?
     
+    enum CodingKeys: String, CodingKey {
+        case post = "post"
+        case creator = "creator"
+        case community = "community"
+        case creatorBannedFromCommunity = "creator_banned_from_community"
+        case counts = "counts"
+        case subscribed = "subscribed"
+        case saved = "saved"
+        case read = "read"
+        case myVote = "my_vote"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        post = try values.decodeIfPresent(LemmyPostItem.self, forKey: .post)
+        creator = try values.decodeIfPresent(LemmyUser.self, forKey: .creator)
+        creatorBannedFromCommunity = try values.decodeIfPresent(Bool.self, forKey: .creatorBannedFromCommunity)
+        counts = try values.decode(Counts.self, forKey: .counts)
+        subscribed = try values.decodeIfPresent(Bool.self, forKey: .subscribed)
+        saved = try values.decodeIfPresent(Bool.self, forKey: .saved)
+        read = try values.decodeIfPresent(Bool.self, forKey: .read)
+        myVote = try values.decodeIfPresent(Bool.self, forKey: .myVote)
+    }
+    
     struct Counts: Codable {
-        // TODO: CodingKeys
         var id: Int
-        var postID: Int?
+        var postID: Int
         var comments: Int?
         var score: Int?
         var upvotes: Int?
@@ -30,5 +52,30 @@ struct LemmyPostItemSummary: Codable {
         var stickied: Bool?
         var published: String?
         var newestCommentTime: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case id = "id"
+            case postID = "post_id"
+            case comments = "comments"
+            case score = "score"
+            case upvotes = "upvotes"
+            case downvotes = "downvotes"
+            case stickied = "stickied"
+            case published = "published"
+            case newestCommentTime = "newest_comment_time"
+        }
+        
+        init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            id = try values.decode(Int.self, forKey: .id)
+            postID = try values.decode(Int.self, forKey: .postID)
+            comments = try values.decodeIfPresent(Int.self, forKey: .comments)
+            score = try values.decodeIfPresent(Int.self, forKey: .score)
+            upvotes = try values.decodeIfPresent(Int.self, forKey: .upvotes)
+            downvotes = try values.decodeIfPresent(Int.self, forKey: .downvotes)
+            stickied = try values.decodeIfPresent(Bool.self, forKey: .stickied)
+            published = try values.decodeIfPresent(String.self, forKey: .published)
+            newestCommentTime = try values.decodeIfPresent(String.self, forKey: .newestCommentTime)
+        }
     }
 }
