d1bedf82169a477e07c751d2e687fd7c47dfabcc
Avery Pierce <avery.pierce@pm.me>
2021-02-07 13:16:32 -0600

    Add a caching data provider

---
+++ Cavy/Utils/Resource/CachingDataProvider.swift
@@ -0,0 +1,69 @@
+//
+//  CachingDataProvider.swift
+//  Cavy
+//
+//  Created by Avery Pierce on 2/7/21.
+//
+
+import Foundation
+
+protocol Cachable {
+    var filename: String { get }
+}
+
+struct CachingDataProvider<CachableDataProvider: DataProvider & Cachable>: DataProvider {
+    let dataProvider: CachableDataProvider
+    init(_ dataProvider: CachableDataProvider) {
+        self.dataProvider = dataProvider
+    }
+    
+    func getData(_ completion: @escaping (Result<Data, Error>) -> Void) {
+        if isCached {
+            getDataFromCache(completion)
+        } else {
+            dataProvider.getData { (result) in
+                if let downloadedData = try? result.get() {
+                    writeToCache(data: downloadedData)
+                }
+                
+                completion(result)
+            }
+        }
+    }
+    
+    func getDataFromCache(_ completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let data = FileManager.default.contents(atPath: cachePath) else {
+            completion(.failure(PlainError("File didn't exist at \(cachePath)")))
+            return
+        }
+        
+        completion(.success(data))
+    }
+    
+    func writeToCache(data: Data) {
+        createFolderIfNeeded()
+        FileManager.default.createFile(atPath: cachePath, contents: data, attributes: nil)
+    }
+    
+    var isCached: Bool {
+        return FileManager.default.fileExists(atPath: cachePath)
+    }
+    
+    var cachePath: String {
+        cachingDataProviderFolderURL.appendingPathComponent(dataProvider.filename).path
+    }
+    
+    var cachingDataProviderFolderURL: URL { Self.cachingDataProviderFolderURL }
+    
+    static var cachingDataProviderFolderURL: URL {
+        let cachePaths = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+        guard cachePaths.count > 0 else { fatalError("Could not find the cache path!") }
+        return cachePaths[0].appendingPathComponent("CachingDataProvider")
+    }
+    
+    func createFolderIfNeeded() { Self.createFolderIfNeeded() }
+    
+    static func createFolderIfNeeded() {
+        try? FileManager.default.createDirectory(at: cachingDataProviderFolderURL, withIntermediateDirectories: true, attributes: nil)
+    }
+}
---
+++ CavyTests/CachingDataProviderTests.swift
@@ -0,0 +1,106 @@
+//
+//  CachingDataProviderTests.swift
+//  CavyTests
+//
+//  Created by Avery Pierce on 2/7/21.
+//
+
+import XCTest
+@testable import Cavy
+
+class CachingDataProviderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try? deleteCacheDirectory()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try? deleteCacheDirectory()
+    }
+
+    func testDataIsWrittenToCache() throws {
+        let filename = "testDataIsWrittenToCache.data"
+        let contents = "Hello World!".data(using: .utf8)!
+        
+        let e = expectation(description: "Cachable")
+        
+        let testDataProvider = TestCachableDataProvider(contents, filename: filename)
+        let cachingDataProvider = CachingDataProvider(testDataProvider)
+        cachingDataProvider.getData { (result) in
+            
+            do {
+                let paths = try self.filesInCacheDirectory()
+                XCTAssertEqual(paths, [filename])
+            } catch let error {
+                XCTFail(error.localizedDescription)
+            }
+            
+            let cachedData = FileManager.default.contents(atPath: self.cacheFolderURL.appendingPathComponent(filename).path)
+            XCTAssertEqual(cachedData, contents)
+            
+            e.fulfill()
+        }
+        
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+    
+    func testDataIsReadFromCache() throws {
+        let filename = "testDataIsReadFromCache.data"
+        let contentsCached = "This data was cached".data(using: .utf8)!
+        let contentsLoaded = "This data was loaded".data(using: .utf8)!
+        
+        let e = expectation(description: "Cachable")
+        
+        // Call this first to make sure the data is cached
+        CachingDataProvider(TestCachableDataProvider(contentsCached, filename: filename)).getData { _ in
+            CachingDataProvider(TestCachableDataProvider(contentsLoaded, filename: filename)).getData { result in
+                let resultData = try? result.get()
+                XCTAssertEqual(resultData, contentsCached)
+                e.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+    
+    var cacheFolderURL: URL {
+        CachingDataProvider<TestCachableDataProvider>.cachingDataProviderFolderURL
+    }
+    
+    func filesInCacheDirectory() throws -> [String] {
+        try FileManager.default.contentsOfDirectory(atPath: cacheFolderURL.path)
+    }
+    
+    func deleteCacheDirectory() throws {
+        let allFiles = try filesInCacheDirectory()
+        for file in allFiles {
+            try FileManager.default.removeItem(at: cacheFolderURL.appendingPathComponent(file))
+        }
+        
+        try FileManager.default.removeItem(at: cacheFolderURL)
+    }
+    
+    struct TestCachableDataProvider: Cachable, Cavy.DataProvider {
+        let result: Result<Data, Error>
+        let filename: String
+        
+        init(_ result: Result<Data, Error>, filename: String) {
+            self.result = result
+            self.filename = filename
+        }
+        
+        init(_ data: Data, filename: String) {
+            self.init(.success(data), filename: filename)
+        }
+        
+        init(_ string: String, filename: String) {
+            self.init(string.data(using: .utf8)!, filename: filename)
+        }
+        
+        func getData(_ completion: @escaping (Result<Data, Error>) -> Void) {
+            completion(result)
+        }
+    }
+}
