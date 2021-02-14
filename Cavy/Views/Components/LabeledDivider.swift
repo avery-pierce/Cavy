7e3646a28cbc4534d7cf1f35e0c7306167df836b
Avery Pierce <avery.pierce@pm.me>
2021-02-14 09:23:53 -0600

    Update onboardinatng

---
+++ Cavy/Views/Components/LabeledDivider.swift
@@ -0,0 +1,42 @@
+//
+//  LabeledDivider.swift
+//  Cavy
+//
+//  Created by Avery Pierce on 2/14/21.
+//
+
+import SwiftUI
+
+struct LabeledDivider<Children: View>: View {
+    var children: Children
+    init(@ViewBuilder _ content: @escaping () -> Children) {
+        self.children = content()
+    }
+    
+    init(content: Children) {
+        self.children = content
+    }
+    
+    var body: some View {
+        HStack {
+            Rectangle().frame(height: 1)
+            children
+            Rectangle().frame(height: 1)
+        }
+        .foregroundColor(Color.gray)
+        .font(.system(.footnote))
+    }
+}
+
+struct LabeledDivider_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            
+            LabeledDivider {
+                Text("OR")
+            }
+            
+            LabeledDivider(content: Text("AND"))
+        }
+    }
+}
--- Cavy/Views/Screens/Onboarding/ChooseInstancePage.swift
+++ Cavy/Views/Screens/Onboarding/ChooseInstancePage.swift
@@ -11,30 +11,39 @@ struct ChooseInstancePage: View {
     @State var lemmyServer: String = ""
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Select an instance")
-            
-            HStack {
-                VStack(alignment: .leading, spacing: 8.0) {
-                    Text("lemmy.ml")
-                        .font(.headline)
-                    Text("The flagship lemmy server. If you've never used Lemmy before, this is a good place to start.")
-                        .font(.subheadline)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Select an instance")
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        HStack {
+                            Text("lemmy.ml")
+                                .font(.headline)
+                            
+                            Text("(recommended)")
+                                .opacity(0.5)
+                        }
+                        Text("The flagship lemmy server. If you've never used Lemmy before, this is a good place to start.")
+                            .font(.subheadline)
+                    }
+                    Spacer()
                 }
-                Spacer()
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.blue, lineWidth: 2)
+                )
+                
+                LabeledDivider(content: Text("OR")).padding(.vertical)
+                
+                Text("Enter another intance")
+                TextField("lemmy.ml", text: $lemmyServer)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 2))
             }
             .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color.blue, lineWidth: 2)
-            )
-            
-            Divider().padding(.vertical)
-            
-            Text("Enter another intance")
-            TextField("lemmy.ml", text: $lemmyServer)
         }
-        .padding()
     }
 }
 
