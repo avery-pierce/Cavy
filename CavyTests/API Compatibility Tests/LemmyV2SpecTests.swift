cd1cb6e188c5e56b47e1171a8091f985b947292a
Avery Pierce <avery.pierce@pm.me>
2021-03-26 10:45:04 -0500

    Refactor LemmyV2SpecTests

--- CavyTests/API Compatibility Tests/LemmyV2SpecTests.swift
+++ CavyTests/API Compatibility Tests/LemmyV2SpecTests.swift
@@ -20,57 +20,48 @@ class LemmyV2SpecTests: XCTestCase {
         try XCTSkipUnless(credentials != nil, "username and password not found in secrets.json")
         
         let (username, password) = credentials!
-        let e = expectation(description: "Login")
-        let spec = client.login(usernameOrEmail: username, password: password)
-        assertDecodes(spec) {
-            e.fulfill()
+        expectWithAnonV2Client("Login") { client, onComplete in
+            let spec = client.login(usernameOrEmail: username, password: password)
+            assertDecodes(spec) {
+                onComplete(nil)
+            }
         }
-        
-        waitForExpectations(timeout: 5, handler: nil)
     }
     
     func testListCommunities() throws {
-        let e = expectation(description: "List Communities")
-        let spec = client.listCommunities(sort: .hot)
-        assertDecodes(spec, printData: true) {
-            e.fulfill()
+        expectWithAnonV2Client("List Communities") { client, onComplete in
+            let spec = client.listCommunities(sort: .hot)
+            assertDecodes(spec, printData: true) {
+                onComplete(nil)
+            }
         }
-        
-        waitForExpectations(timeout: 5, handler: nil)
     }
     
     func testListPosts() throws {
-        let e = expectation(description: "List Posts")
-        let spec = client.listPosts(type: .all, sort: .hot)
-        assertDecodes(spec) {
-            e.fulfill()
+        expectWithAnonV2Client("List Posts") { client, onComplete in
+            let spec = client.listPosts(type: .all, sort: .hot)
+            assertDecodes(spec) {
+                onComplete(nil)
+            }
         }
-        
-        waitForExpectations(timeout: 5, handler: nil)
     }
     
     func testListPostsAuthed() throws {
-        
-        let e = expectation(description: "list posts")
-        activeSession.vend { (authClient) in
-            guard let authClient = authClient, case .v2(let client) = authClient else { return XCTFail() }
+        expectWithAuthedV2Client("List Authed Posts") { client, onComplete in
             let spec = client.listPosts(type: .subscribed, sort: .hot)
             assertDecodes(spec) {
-                e.fulfill()
+                onComplete(nil)
             }
         }
-        
-        waitForExpectations(timeout: 5, handler: nil)
     }
     
     func testListPostsByCommunity() throws {
-        let e = expectation(description: "List Posts")
-        let spec = client.listPosts(type: .all, sort: .hot, communityID: 1)
-        assertDecodes(spec) {
-            e.fulfill()
+        expectWithAnonV2Client("List Posts") { client, onComplete in
+            let spec = client.listPosts(type: .all, sort: .hot, communityID: 1)
+            assertDecodes(spec) {
+                onComplete(nil)
+            }
         }
-        
-        waitForExpectations(timeout: 5, handler: nil)
     }
 
     func testListComments() throws {
@@ -79,76 +70,93 @@ class LemmyV2SpecTests: XCTestCase {
          Post ID: 41391
          post ID: 41326
          */
-        let e = expectation(description: "Get post")
-        let spec = client.fetchPost(id: 1)
-        assertDecodes(spec) {
-            e.fulfill()
+        expectWithAnonV2Client("Get Post") { client, onComplete in
+            let spec = client.fetchPost(id: 1)
+            assertDecodes(spec) {
+                onComplete(nil)
+            }
         }
-        
-        waitForExpectations(timeout: 5, handler: nil)
     }
 
     func testGetSiteData() throws {
-        let e = expectation(description: "Fetch Site")
-        let spec = client.fetchSite()
-        assertDecodes(spec) {
-            e.fulfill()
+        expectWithAnonV2Client("Fetch Site") { client, onComplete in
+            let spec = client.fetchSite()
+            assertDecodes(spec) {
+                onComplete(nil)
+            }
         }
-        
-        waitForExpectations(timeout: 5, handler: nil)
     }
     
     func testVote() throws {
-        let e = expectation(description: "Vote")
-        activeSession.vend { (authClient) in
-            guard let authClient = authClient, case .v2(let client) = authClient else { return XCTFail() }
+        expectWithAuthedV2Client("Vote") { client, onComplete in
             let spec = client.vote(1, onPostID: 1)
             assertDecodes(spec, printData: true) {
-                e.fulfill()
+                onComplete(nil)
             }
         }
-        
-        waitForExpectations(timeout: 10, handler: nil)
     }
     
     func testSubmitPost() throws {
-        let e = expectation(description: "Create Post")
-        activeSession.vend { (authClient) in
-            guard let authClient = authClient, case .v2(let client) = authClient else { return XCTFail() }
+        expectWithAuthedV2Client("Create Post") { client, onComplete in
             let spec = client.submitPost(name: "Automated test post \(Date().description)", url: "https://example.com", body: "This is a test post", nsfw: false, communityID: 2)
             assertDecodes(spec, printData: true) {
-                e.fulfill()
+                onComplete(nil)
             }
         }
-        
-        waitForExpectations(timeout: 10, handler: nil)
     }
     
     func testSubmitComment() throws {
-        let e = expectation(description: "Submit Comment")
-        activeSession.vend { (authClient) in
-            guard let authClient = authClient, case .v2(let client) = authClient else { return XCTFail() }
+        expectWithAuthedV2Client("Submit Comment") { (client, onComplete) in
             let spec = client.submitComment(content: "Hello world! This is an automated comment \(Date().description)", postID: 1, parentID: nil)
             
             assertDecodes(spec, printData: true) {
-                e.fulfill()
+                onComplete(nil)
             }
         }
-        
-        waitForExpectations(timeout: 10, handler: nil)
     }
     
     func testVoteComment() throws {
-        let e = expectation(description: "Vote Comment")
-        activeSession.vend { (authClient) in
-            guard let authClient = authClient, case .v2(let client) = authClient else { return XCTFail() }
+        expectWithAuthedV2Client("Vote Comment") { (client, onComplete) in
             let spec = client.vote(1, onCommentID: 1)
             
             assertDecodes(spec, printData: true) {
+                onComplete(nil)
+            }
+        }
+    }
+    
+    func withAuthedV2Client(_ block: @escaping (LemmyV2Spec) -> Void) {
+        activeSession.vend { (authClient) in
+            guard let authClient = authClient, case .v2(let client) = authClient else { return XCTFail() }
+            block(client)
+        }
+    }
+    
+    func expect(_ name: String, timeout: TimeInterval = 10, _ block: @escaping (_ completion: @escaping (Error?) -> Void) -> Void) {
+        let e = expectation(description: name)
+        
+        block { error in
+            if let error = error {
+                XCTFail(error.localizedDescription)
+            } else {
                 e.fulfill()
             }
         }
         
-        waitForExpectations(timeout: 10, handler: nil)
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+    
+    func expectWithAuthedV2Client(_ name: String, timeout: TimeInterval = 10, _ block: @escaping (LemmyV2Spec, @escaping (Error?) -> Void) -> Void) {
+        expect(name, timeout: timeout) { onComplete in
+            self.withAuthedV2Client { client in
+                block(client, onComplete)
+            }
+        }
+    }
+    
+    func expectWithAnonV2Client(_ name: String, timeout: TimeInterval = 10, _ block: @escaping (LemmyV2Spec, @escaping (Error?) -> Void) -> Void) {
+        expect(name, timeout: timeout) { onComplete in
+            block(self.client, onComplete)
+        }
     }
 }
